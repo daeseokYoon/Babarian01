7c17879c671d6fec23d7815f63bd2d9801f09899
daeseokYoon <dbseotjr4122@naver.com>
2023-11-15 09:22:08 +0900

    feat.BattleBase 만들기

---
+++ Assets/Scripts/BaseNPCStatus.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+[System.Serializable]
+public class BaseNPCStatus
+{
+    public string name;
+
+    public enum Rarity
+    {
+        COMMON,
+        UNCOMMON,
+        RARE,
+        SUPERRARE,
+    }
+
+    public Rarity rarity;
+
+    public float baseHP;
+    public float curHP;
+
+
+}
---
+++ Assets/Scripts/BasePlayerStatus.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BasePlayerStatus
+{
+    public string name;
+
+    public float baseHP;
+    public float curHP;
+
+    public int stamina;
+    public int intellect;
+    public int dexterity;
+    public int agility;
+}
